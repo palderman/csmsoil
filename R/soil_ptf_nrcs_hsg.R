11d49fea948dc3f418c8067846c07d021e64d27f
Phillip D. Alderman <phillip.alderman@okstate.edu>
2025-10-21 11:41:34 -0500

    Updating TODO and placeholder for soil_ptf_nrcs_hsg()

---
+++ R/soil_ptf_nrcs_hsg.R
@@ -0,0 +1,11 @@
+#' Determine hydrologic soil group
+#'
+#' Determine hydrologic soil group (HSG) based on saturated conductivity
+#'  with depth and the method described in Chapter 7 - "Hydrologic Soil
+#'   Groups" of the NRCS Part 630 National Engineering Handbook (2009)
+#'
+#' @export
+#'
+soil_ptf_nrcs_hsg <- function(ssks, depth){
+
+}
